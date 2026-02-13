538a48e68caa087bb0746a3ec1bb85e157e1119f
Ian Alvarez <iaag0001@ce.pucmm.edu.do>
2026-02-13 16:16:39 -0400

    Estructurando inicialmente el proyecto:

--- src/main/java/edu/pucmm/icc352/Main.java
+++ src/main/java/edu/pucmm/icc352/Main.java
@@ -1,8 +1,30 @@
 package edu.pucmm.icc352;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Creando la instancia del servidor y configurando.
+        Javalin app = Javalin.create(config -> {
+
+            // configurando los documentos estáticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+            });
+
+        }).start(7000);
+
+        app.get("/", ctx -> {
+            ctx.contentType("text/plain; charset=UTF-8");
+            ctx.result("Blog funcionando 🚀");
+        });
+
     }
 }
-
